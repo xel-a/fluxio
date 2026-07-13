7829b1a03b1e9161c011cb9ace244375849663cb
Axel Ignacio <ignacioaxelrom@gmail.com>
2026-07-13 12:15:10 -0400

    refactor(main.go)!: move main.go to /cmd/api
    BREAKING CHANGE: The configuration files such as .air.toml and
    dockerfile must now point to /cmd/api.

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fluxio/internal/app"
+	"fluxio/internal/config"
+	"fluxio/internal/database"
+	"fluxio/internal/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	cfg := config.Load()
+	db, err := database.New(cfg.DatabaseURL, context.Background())
+
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+
+	handlers := app.Initialize(db)
+	router := routes.NewRouter(handlers)
+	
+	log.Println("Server listening on Port: ", cfg.Port)
+
+	if err := http.ListenAndServe(":" + cfg.Port, router); err != nil {
+		log.Fatal(err)
+	}
+}
